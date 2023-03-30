9b9a964d7a97bc01c0b51c31b5afe2f36e5fe904
Alex Zenla <alex@fork.run>
2023-03-30 12:07:06 -0700

    universal support for archives

--- build.sh
+++ build.sh
@@ -20,8 +20,10 @@ then
   git submodule update --init --recursive
 fi
 
-mkdir -p "build"
+mkdir -p "libs"
+mkdir -p "archives"
 
 cd "libtailscale"
-CGO_ENABLED=1 go build -buildmode=c-shared -o "../build/libtailscale-${GOOS}-${GOARCH}.${LIBSUFFIX}"
-rm -rf "../build/"*.h
+CGO_ENABLED=1 go build -buildmode=c-shared -o "../libs/libtailscale-${GOOS}-${GOARCH}.${LIBSUFFIX}"
+CGO_ENABLED=1 go build -buildmode=c-archive -o "../archives/libtailscale-${GOOS}-${GOARCH}.a"
+rm -rf "../libs/"*.h "../archives/"*.h
--- universal.sh
+++ universal.sh
@@ -5,10 +5,13 @@ OUT="${1}"
 shift
 
 LIBS=()
+ARCHIVES=()
 
 for PLATFORM in ${@}
 do
-  LIBS+=("build/libtailscale-${PLATFORM}.dylib")
+  LIBS+=("libs/libtailscale-${PLATFORM}.dylib")
+  ARCHIVES+=("archives/libtailscale-${PLATFORM}.a")
 done
 
-lipo -create -output "build/libtailscale-${OUT}.dylib" "${LIBS[@]}"
+lipo -create -output "libs/libtailscale-${OUT}.dylib" "${LIBS[@]}"
+lipo -create -output "archives/libtailscale-${OUT}.a" "${ARCHIVES[@]}"
